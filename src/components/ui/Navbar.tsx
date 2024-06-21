766f7b9902f6a580aad8d559454e20ad0a0e07cf
Yujiii0 <yuji918@naver.com>
2024-06-21 20:52:22 +0900

    ✨ feat: 회원탈퇴 기능 구현

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,9 +1,10 @@
 import { auth } from "@/api/firebase";
 import { FirebaseError } from "firebase/app";
-import { signOut } from "firebase/auth";
+import { deleteUser, signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 
 function Navbar() {
+  const user = auth.currentUser;
   const navigate = useNavigate();
   const handleClickLogOut = async () => {
     try {
@@ -17,10 +18,22 @@ function Navbar() {
       }
     }
   };
+  const handleClickDeleteAccount = async () => {
+    try {
+      if (user) {
+        await deleteUser(user);
+        alert("그동안 멍냥생활을 이용해주셔서 감사합니다.");
+        navigate("/");
+      }
+    } catch (error) {
+      alert("오류가 발생했습니다. 다시 시도해주세요");
+    }
+  };
 
   return (
     <div>
       <button onClick={handleClickLogOut}>로그아웃</button>
+      <button onClick={handleClickDeleteAccount}>회원 탈퇴</button>
     </div>
   );
 }
