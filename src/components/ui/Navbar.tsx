79fdd5c311c69da5eb6d5befddc9a1d97e2e19da
Yujiii0 <yuji918@naver.com>
2024-06-21 20:51:51 +0900

    ✨ feat: 로그아웃 기능 구현

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,5 +1,28 @@
+import { auth } from "@/api/firebase";
+import { FirebaseError } from "firebase/app";
+import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
 function Navbar() {
-  return <div>Navbar</div>;
+  const navigate = useNavigate();
+  const handleClickLogOut = async () => {
+    try {
+      await signOut(auth);
+      navigate("/");
+    } catch (error) {
+      if (error instanceof FirebaseError) {
+        alert(error.code || "로그인에 실패하였습니다.");
+      } else {
+        alert("오류가 발생했습니다. 다시 시도해주세요");
+      }
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleClickLogOut}>로그아웃</button>
+    </div>
+  );
 }
 
 export default Navbar;
