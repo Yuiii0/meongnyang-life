c0ca9a88bd536c4c64a5e6ee76e5fecdcb0af5fd
Yujiii0 <yuji918@naver.com>
2024-06-21 20:44:06 +0900

    ✨ firebase auth setting

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-
+import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
@@ -11,4 +11,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export default app;
+
+export const auth = getAuth(app);
