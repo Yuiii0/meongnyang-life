231a9d96304433c860fdec9026d5eda1ac6c1d15
Yujiii0 <yuji918@naver.com>
2024-06-21 20:48:48 +0900

    🏷️ type: authErrorMessage Type 정의

---
+++ src/api/authErrorMessages.ts
@@ -0,0 +1,17 @@
+export const authErrorMessages: { [key: string]: string } = {
+  "auth/invalid-email": "유효하지 않은 이메일 주소입니다",
+  "auth/user-disabled": "비활성화된 사용자 입니다",
+  "auth/user-not-found": "존재하지 않는 사용자 입니다",
+  "auth/wrong-password": "비밀번호가 올바르지 않습니다",
+  "auth/too-many-requests":
+    "너무 많은 로그인 시도가 있었습니다. 잠시 후 다시 시도해주세요",
+  "auth/network-request-failed":
+    "네트워크 오류가 발생했습니다. 인터넷 연결 상태를 확인하고 다시 시도해주세요",
+  "auth/operation-not-allowed":
+    "이메일/비밀번호 로그인 기능이 비활성화되어 있습니다. 관리자에게 문의하세요",
+  "auth/weak-password":
+    "비밀번호가 너무 약합니다. 더 강력한 비밀번호를 사용해주세요",
+  "auth/email-already-in-use": "이미 존재하는 이메일 입니다",
+  "auth/invalid-password": "비밀번호가 유효하지 않습니다 ",
+  "auth/invalid-credential": "로그인에 실패하였습니다. 다시 시도해주세요",
+};
