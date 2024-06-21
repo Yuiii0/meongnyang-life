334ea2d4e6d7dce30c357f2fc1249a860bf39a6b
Yujiii0 <yuji918@naver.com>
2024-06-21 20:47:12 +0900

    ✨ feat: 로그인 페이지 구현

--- src/pages/LogIn/LogInPage.tsx
+++ src/pages/LogIn/LogInPage.tsx
@@ -1,5 +1,15 @@
+import LogInForm from "@/components/pages/logIn/LogInForm";
+import { Link } from "react-router-dom";
+
 function LogInPage() {
-  return <div>LogIn</div>;
+  return (
+    <div>
+      <h1>🐾 멍냥 생활</h1>
+      <LogInForm />
+      <Link to="/signup">계정 만들기</Link>
+      <Link to="/find/pw">비밀번호 재설정</Link>
+    </div>
+  );
 }
 
 export default LogInPage;
