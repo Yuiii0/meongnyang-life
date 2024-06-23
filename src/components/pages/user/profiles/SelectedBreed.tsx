3a7ab40cbc969076b6950065e58e0cde1ec237f2
Yujiii0 <yuji918@naver.com>
2024-06-23 22:15:39 +0900

    ✨ feat: SelectedBreed 구현

---
+++ src/components/pages/user/profiles/SelectedBreed.tsx
@@ -0,0 +1,95 @@
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { catBreedData } from "@/data/breeds/catbreed.data";
+import { dogBreedData } from "@/data/breeds/dogbreed.data";
+import { cn } from "@/lib/utils";
+import { Check, ChevronsUpDown } from "lucide-react";
+import { useEffect, useState } from "react";
+
+interface SelectedBreedsProps {
+  petType?: string;
+  breed: string;
+  disabled?: boolean;
+  handleChangeBreed: (breed: string) => void;
+}
+
+export function SelectedBreed({
+  petType = "dog",
+  breed,
+  disabled = false,
+  handleChangeBreed,
+}: SelectedBreedsProps) {
+  const [open, setOpen] = useState(false);
+  const breedData = petType === "cat" ? catBreedData : dogBreedData;
+  const petTypeKr = petType === "cat" ? "묘종" : "견종";
+
+  const handleSelectBreed = (currentValue: string) => {
+    handleChangeBreed(currentValue);
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    handleChangeBreed("");
+  }, [petType]);
+
+  return (
+    <div>
+      <h4 className="py-3 text-sm font-semibold ">견종/묘종</h4>
+      <div className="flex justify-center">
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger asChild>
+            <Button
+              variant="outline"
+              role="combobox"
+              aria-expanded={open}
+              disabled={disabled}
+              className="w-[200px] justify-between"
+            >
+              {breed
+                ? breedData.find((b) => b.value === breed)?.label
+                : `${petTypeKr}을 선택해주세요`}
+              <ChevronsUpDown className="w-4 h-4 ml-2 opacity-50 shrink-0" />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-[200px] p-0 ">
+            <Command>
+              <CommandInput placeholder={`${petTypeKr}을 검색해보세요`} />
+              <CommandList className="overflow-y-auto max-h-32">
+                <CommandEmpty>{`해당하는 ${petTypeKr}이 없습니다`}</CommandEmpty>
+                <CommandGroup>
+                  {breedData.map((b) => (
+                    <CommandItem
+                      key={b.value}
+                      value={b.value}
+                      onSelect={() => handleSelectBreed(b.value)}
+                    >
+                      <Check
+                        className={cn(
+                          "mr-2 h-4 w-4",
+                          breed === b.value ? "opacity-100" : "opacity-0"
+                        )}
+                      />
+                      {b.label}
+                    </CommandItem>
+                  ))}
+                </CommandGroup>
+              </CommandList>
+            </Command>
+          </PopoverContent>
+        </Popover>
+      </div>
+    </div>
+  );
+}
