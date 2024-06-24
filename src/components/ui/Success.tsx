843b9bcc09fa9f435eab33a9e92c13e8c31d8f5a
Yujiii0 <yuji918@naver.com>
2024-06-24 14:02:56 +0900

    ✨ feat: Success 컴포넌트 생성

---
+++ src/components/ui/Success.tsx
@@ -0,0 +1,33 @@
+import { PropsWithChildren } from "react";
+import NavigationLink from "./NavigationLink";
+
+interface SuccessProps {
+  linkText?: string;
+  text?: string;
+}
+
+function Success({
+  children,
+  linkText,
+  text,
+}: PropsWithChildren<SuccessProps>) {
+  return (
+    <div className="text-center ">
+      <div className="fixed flex flex-col transform -translate-x-1/2 -translate-y-2/3 top-1/2 left-1/2 gap-y-5">
+        <div className="w-[300px] h-[300px]  bg-orange-100 rounded-full flex items-center justify-center">
+          <img
+            src="/public/images/family.png"
+            alt="family"
+            className="items-center justify-center w-[220px] mx-auto h-[220px]"
+          />
+        </div>
+        <div className="text-lg text-gray-400">{text}</div>
+        <div className="text-[42px] font-semibold">{children}</div>
+      </div>
+
+      <NavigationLink to="/main">{linkText}</NavigationLink>
+    </div>
+  );
+}
+
+export default Success;
