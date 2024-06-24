924a57888dd00872827afd6c824eec86f60e9da0
Yujiii0 <yuji918@naver.com>
2024-06-24 11:46:21 +0900

    💄 design: ErrorMessage css 구현

--- src/components/ui/ErrorMessage.tsx
+++ src/components/ui/ErrorMessage.tsx
@@ -1,5 +1,13 @@
 function ErrorMessage({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
+  return (
+    <div
+      className={`text-xs px-2  ${
+        children ? "text-warning opacity-100" : "opacity-0"
+      } h-2 `}
+    >
+      {children || ""}
+    </div>
+  );
 }
 
 export default ErrorMessage;
