93b7862b9521730f1fc8e006247ae3e70c7e44f7
Yujiii0 <yuji918@naver.com>
2024-06-24 16:16:56 +0900

    💄 design: LogIn 스타일링

--- src/components/pages/logIn/LogInForm.tsx
+++ src/components/pages/logIn/LogInForm.tsx
@@ -4,7 +4,6 @@ import Button from "@/components/ui/Button/Button";
 import { useUserStore } from "@/stores/user/useUserStore";
 
 import { FirebaseError } from "firebase/app";
-import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 
@@ -15,18 +14,17 @@ interface LogInForm {
 
 function LogInForm() {
   const navigate = useNavigate();
-  const [isLoading, setLoading] = useState(false);
+  // const [isLoading, setLoading] = useState(false);
   const { setUser, user } = useUserStore();
   const {
     register,
     handleSubmit,
     formState: { errors },
-    setError,
   } = useForm<LogInForm>();
 
   const onValid = async (data: LogInForm) => {
     try {
-      setLoading(true);
+      // setLoading(true);
       const email = data.email;
       const password = data.password;
 
@@ -42,68 +40,92 @@ function LogInForm() {
         alert("로그인 중 오류가 발생했습니다. 다시 시도해주세요");
       }
     } finally {
-      setLoading(false);
+      // setLoading(false);
     }
   };
 
   return (
     <form onSubmit={handleSubmit(onValid)}>
-      <div className="p-4 bg-white rounded-lg">
-        <div className="relative bg-inherit">
-          <input
-            type="text"
-            id="email"
-            className="h-10 px-2 text-gray-200 placeholder-transparent bg-transparent rounded-lg peer w-72 ring-2 ring-gray-500 focus:ring-sky-600 focus:outline-none focus:border-rose-600"
-            placeholder="이메일을 입력해주세요"
-            {...register("email", {
-              required: "이메일을 입력해주세요",
-              pattern: {
-                value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i,
-                message: "올바른 이메일 형식으로 입력해주세요",
-              },
-            })}
-          />
-          <label
-            htmlFor="email"
-            className="absolute left-0 px-1 mx-1 text-sm text-gray-500 transition-all cursor-text -top-3 bg-inherit peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-placeholder-shown:top-2 peer-focus:-top-3 peer-focus:text-sky-600 peer-focus:text-sm"
-          >
-            이메일
-          </label>
+      <div className="flex flex-col gap-y-10">
+        <div className="bg-white rounded-lg ">
+          <div className="relative bg-inherit">
+            <input
+              type="text"
+              id="email"
+              className={`w-full px-4 text-gray-700 placeholder-transparent bg-transparent rounded-lg h-11 peer ring-1 ring-gray-300 focus:ring-gray-700 focus:outline-none ${
+                errors.email
+                  ? "focus:border-red-500 focus:ring-red-500"
+                  : "focus:border-black focus:ring-gray-700"
+              }`}
+              placeholder="이메일을 입력해주세요"
+              {...register("email", {
+                required: "이메일을 입력해주세요",
+                pattern: {
+                  value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i,
+                  message: "올바른 이메일 형식으로 입력해주세요",
+                },
+              })}
+            />
+            <label
+              htmlFor="email"
+              className={`absolute left-0 px-1 mx-1 text-sm transition-all cursor-text -top-3 bg-inherit peer-placeholder-shown:text-base peer-placeholder-shown:top-2 peer-focus:-top-3 peer-focus:text-sm ${
+                errors.email
+                  ? "text-warning peer-placeholder-shown:text-warning"
+                  : "text-gray-800 peer-placeholder-shown:text-gray-700"
+              }`}
+            >
+              이메일
+            </label>
+          </div>
+          {errors.email && (
+            <p className="px-1 pt-2 text-sm text-warning text-start">
+              {errors.email.message}
+            </p>
+          )}
         </div>
-      </div>
-      <div className="p-4 bg-white rounded-lg">
-        <div className="relative bg-inherit">
-          <input
-            type="password"
-            id="password"
-            className="h-10 px-2 text-gray-200 placeholder-transparent bg-transparent rounded-lg peer w-72 ring-2 ring-gray-500 focus:ring-sky-600 focus:outline-none focus:border-rose-600"
-            placeholder="비밀번호를 입력해주세요"
-            {...register("password", {
-              required: "비밀번호를 입력해주세요",
-              minLength: {
-                value: 8,
-                message: "8글자 이상 입력해주세요",
-              },
-              pattern: {
-                value:
-                  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
-                message: "영문자, 숫자, 특수문자를 포함하여 입력해주세요",
-              },
-            })}
-          />
-          <label
-            htmlFor="password"
-            className="absolute left-0 px-1 mx-1 text-sm text-gray-500 transition-all cursor-text -top-3 bg-inherit peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-placeholder-shown:top-2 peer-focus:-top-3 peer-focus:text-sky-600 peer-focus:text-sm"
-          >
-            비밀번호
-          </label>
+        <div className="mb-4 bg-white rounded-lg">
+          <div className="relative bg-inherit">
+            <input
+              type="password"
+              id="password"
+              className={`w-full px-2 text-gray-700 placeholder-transparent bg-transparent rounded-lg h-11 peer ring-1 ring-gray-300 focus:ring-gray-700 focus:outline-none ${
+                errors.password
+                  ? "focus:border-red-500 focus:ring-red-500"
+                  : "focus:border-black focus:ring-gray-700"
+              }`}
+              placeholder="비밀번호를 입력해주세요"
+              {...register("password", {
+                required: "비밀번호를 입력해주세요",
+                minLength: {
+                  value: 8,
+                  message: "8글자 이상 입력해주세요",
+                },
+                pattern: {
+                  value:
+                    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+                  message: "영문자, 숫자, 특수문자를 포함하여 입력해주세요",
+                },
+              })}
+            />
+            <label
+              htmlFor="password"
+              className={`absolute left-0 px-1 mx-1 text-sm transition-all cursor-text -top-3 bg-inherit peer-placeholder-shown:text-base peer-placeholder-shown:top-2 peer-focus:-top-3 peer-focus:text-sm ${
+                errors.password
+                  ? "text-warning peer-placeholder-shown:text-warning"
+                  : "text-gray-800 peer-placeholder-shown:text-gray-700"
+              }`}
+            >
+              비밀번호
+            </label>
+          </div>
+          {errors.password && (
+            <p className="px-1 pt-2 text-sm text-warning text-start">
+              {errors.password.message}
+            </p>
+          )}
         </div>
-        {errors.password && (
-          <p className="text-sm text-red-500">{errors.password.message}</p>
-        )}
+        <Button>로그인</Button>
       </div>
-      <Button>로그인</Button>
-      <button type="submit"></button>
     </form>
   );
 }
--- src/pages/LogIn/LogInPage.tsx
+++ src/pages/LogIn/LogInPage.tsx
@@ -1,15 +1,26 @@
 import LogInForm from "@/components/pages/logIn/LogInForm";
 import NavigationLink from "@/components/ui/NavigationLink";
+import Page from "@/components/ui/Page";
 import GoogleLogInButton from "./GoogleLogInButton";
 
 function LogInPage() {
   return (
-    <div>
-      <h1>🐾 멍냥 생활</h1>
-      <LogInForm />
-      <GoogleLogInButton />
-      <NavigationLink to="/signup">가입하기</NavigationLink>
-      <NavigationLink to="/find/pw">비밀번호 재설정</NavigationLink>
+    <div className="flex items-center justify-center min-h-screen">
+      <Page>
+        <section className="w-full max-w-md text-center">
+          <div className="py-6 mb-10 text-4xl font-bold ">🐾 멍냥 생활</div>
+          <LogInForm />
+          <GoogleLogInButton />
+          <div className="flex justify-end gap-6 mt-12">
+            <NavigationLink to="/signup" isBottom={false}>
+              가입하기
+            </NavigationLink>
+            <NavigationLink to="/find/pw" isBottom={false}>
+              비밀번호 재설정
+            </NavigationLink>
+          </div>
+        </section>
+      </Page>
     </div>
   );
 }
