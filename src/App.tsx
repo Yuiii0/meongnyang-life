ed946c0faa38c537f10b6272c01ef4f0c9c8cbe1
Yujiii0 <yuji918@naver.com>
2024-06-25 20:02:33 +0900

    ✨ feat: ReactQuery setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/ui/Navbar";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Navbar />
       <Outlet />
-    </>
+    </QueryClientProvider>
   );
 }
 
