d9f4e69ba8173d8f0d01119266e241d97554c725
Yujiii0 <yuji918@naver.com>
2024-06-25 16:18:13 +0900

    ✨ feat: Profile Create 기능 구현

--- src/pages/User/UserProfileCreatePage.tsx
+++ src/pages/User/UserProfileCreatePage.tsx
@@ -1,29 +1,31 @@
 import { auth } from "@/api/auth/auth.api";
+import { db } from "@/api/database/db.api";
 import OptionalProfileForm from "@/components/pages/user/profiles/OptionalProfileForm";
 import RequiredProfileForm from "@/components/pages/user/profiles/RequiredProfileForm";
 import NextButton from "@/components/ui/Button/NextButton";
 import PrevButton from "@/components/ui/Button/PrevButton";
 import Page from "@/components/ui/Page";
 import Success from "@/components/ui/Success";
+import { addDoc, collection } from "firebase/firestore";
 import { ChangeEvent, useState } from "react";
 
 function UserProfileCreatePage() {
+  const user = auth.currentUser;
+
   const [step, setStep] = useState(1);
   const [nickName, setNickName] = useState("");
   const [nickNameErrorMessage, setNickNameErrorMessage] = useState("");
   const [introduction, setIntroduction] = useState("");
   const [introErrorMessage, setIntroErrorMessage] = useState("");
   const [hasError, setHasError] = useState(true);
+  const [profileImg, setProfileImg] = useState(user?.photoURL);
 
   const [petType, setPetType] = useState("");
   const [breed, setBreed] = useState("");
   const [gender, setGender] = useState("");
   const [isNoPet, setIsNoPet] = useState(false);
 
-  const user = auth.currentUser;
-  const photoURL = user?.photoURL;
-  const DEFAULT_PROFILE_IMG = "/images/profile_dog.png";
-
+  //Step 이동 함수
   const handleClickPrevStep = () => {
     setStep((prev) => prev - 1);
   };
@@ -32,6 +34,7 @@ function UserProfileCreatePage() {
     setStep((prev) => prev + 1);
   };
 
+  //RequiredForm handler 함수
   const handleChangeNickName = (e: ChangeEvent<HTMLInputElement>) => {
     const text = e.target.value;
 
@@ -58,17 +61,14 @@ function UserProfileCreatePage() {
     }
   };
 
-  const handleChangePetType = (selectedPet: "dog" | "cat" | "") => {
+  const handleChangeProfileImg = (profileImg: string) =>
+    setProfileImg(profileImg);
+
+  //OptionalForm handler 함수
+  const handleChangePetType = (selectedPet: "dog" | "cat" | "") =>
     setPetType(selectedPet);
-  };
-
-  const handleChangeBreed = (breed: string) => {
-    setBreed(breed);
-  };
-
-  const handleChangeGender = (gender: string) => {
-    setGender(gender);
-  };
+  const handleChangeBreed = (breed: string) => setBreed(breed);
+  const handleChangeGender = (gender: string) => setGender(gender);
 
   const handleSubmitProfile = async () => {
     const user = auth.currentUser;
@@ -76,8 +76,26 @@ function UserProfileCreatePage() {
       alert("접근 권한이 없습니다");
       return;
     }
+    try {
+      await addDoc(collection(db, "users"), {
+        userId: user.uid,
+        userName: user.displayName || "Anonymous",
+        profileImg,
+        email: user.email,
+        nickName,
+        introduction,
+        petType,
+        breed,
+        gender,
+        createdAt: Date.now(),
+      });
+      handleClickNextStep();
+    } catch (error) {
+      alert("오류가 발생했습니다. 다시 시도해주세요");
+    }
   };
 
+  // 다음 단계로 넘어가기 전에 validation 검사
   const handleNextButtonClick = async (e: React.FormEvent) => {
     e.preventDefault();
     let localHasError = false;
@@ -114,10 +132,10 @@ function UserProfileCreatePage() {
             introduction={introduction}
             nickNameErrorMessage={nickNameErrorMessage}
             introErrorMessage={introErrorMessage}
-            photoURL={photoURL}
-            DEFAULT_PROFILE_IMG={DEFAULT_PROFILE_IMG}
+            profileImg={profileImg}
             handleChangeNickName={handleChangeNickName}
             handleChangeIntro={handleChangeIntro}
+            handleChangeProfileImg={handleChangeProfileImg}
             handleNextButtonClick={handleNextButtonClick}
           />
         </>
