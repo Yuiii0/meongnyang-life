63137f6334012596d95018cfcec9bceba2350cd4
Yujiii0 <yuji918@naver.com>
2024-06-25 16:17:32 +0900

    ✨ feat: ProfileImg 업데이트 기능 구현

--- src/components/pages/user/profiles/RequiredProfileForm.tsx
+++ src/components/pages/user/profiles/RequiredProfileForm.tsx
@@ -1,7 +1,11 @@
+import { auth } from "@/api/auth/auth.api";
+import { storage } from "@/api/store/store.api";
 import NextButton from "@/components/ui/Button/NextButton";
 import ErrorMessage from "@/components/ui/ErrorMessage";
 import Input from "@/components/ui/Input/Input";
 import TextArea from "@/components/ui/Input/TextArea";
+import { updateProfile } from "firebase/auth";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { Pencil } from "lucide-react";
 import { ChangeEvent, useEffect, useRef } from "react";
 
@@ -10,9 +14,9 @@ interface RequiredProfileFormProps {
   introduction: string;
   nickNameErrorMessage: string;
   introErrorMessage: string;
-  photoURL?: string | null;
-  DEFAULT_PROFILE_IMG: string;
+  profileImg?: string | null;
   handleChangeNickName: (e: ChangeEvent<HTMLInputElement>) => void;
+  handleChangeProfileImg: (profileImg: string) => void;
   handleChangeIntro: (e: ChangeEvent<HTMLTextAreaElement>) => void;
   handleNextButtonClick: (e: React.FormEvent) => void;
 }
@@ -22,14 +26,17 @@ function RequiredProfileForm({
   introduction,
   nickNameErrorMessage,
   introErrorMessage,
-  photoURL,
-  DEFAULT_PROFILE_IMG,
+  profileImg,
   handleChangeNickName,
   handleChangeIntro,
+  handleChangeProfileImg,
   handleNextButtonClick,
 }: RequiredProfileFormProps) {
   const nickNameRef = useRef<HTMLInputElement>(null);
   const introRef = useRef<HTMLTextAreaElement>(null);
+
+  const user = auth.currentUser;
+  const DEFAULT_PROFILE_IMG = "/images/profile_dog.png";
 
   useEffect(() => {
     if (nickNameErrorMessage && nickNameRef.current) {
@@ -39,23 +46,57 @@ function RequiredProfileForm({
     }
   }, [nickNameErrorMessage, introErrorMessage]);
 
-  const handleClickEditProfileImg = () => {};
+  const handleClickEditProfileImg = async (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const { files } = e.target;
+
+    if (!user) {
+      alert("접근 권한이 없습니다");
+      return;
+    }
+    if (files && files.length == 1) {
+      const file = files[0];
+      if (file) {
+        const locationRef = ref(storage, `users/${user?.uid}`);
+        const result = await uploadBytes(locationRef, file);
+        const profileUrl = await getDownloadURL(result.ref);
+        handleChangeProfileImg(profileUrl);
+
+        try {
+          await updateProfile(user, {
+            photoURL: profileUrl,
+          });
+        } catch (error) {
+          alert("오류가 발생했습니다. 다시 시도해주세요");
+        }
+      }
+    }
+  };
 
   return (
     <form className="flex flex-col gap-y-8" onSubmit={handleNextButtonClick}>
-      <div
+      <label
+        htmlFor="profileImg"
         className="relative flex items-center justify-center w-24 h-24 mx-auto cursor-pointer"
-        onClick={handleClickEditProfileImg}
       >
         <img
-          src={photoURL || DEFAULT_PROFILE_IMG}
+          src={profileImg || DEFAULT_PROFILE_IMG}
           alt="profile-img"
           className="w-full h-full text-center"
         />
         <div className="absolute bottom-0 right-0 flex items-center justify-center w-5 h-5 bg-gray-200 rounded-full">
           <Pencil size={12} />
         </div>
-      </div>
+      </label>
+      <input
+        id="profileImg"
+        type="file"
+        accept="image/*"
+        className="hidden"
+        onChange={handleClickEditProfileImg}
+      />
+      <label />
       <div className="flex flex-col gap-y-10">
         <div>
           <Input
