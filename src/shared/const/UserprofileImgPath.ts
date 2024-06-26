93791547d245b18fde05f0a643ea612e348c073d
Yujiii0 <yuji918@naver.com>
2024-06-26 21:03:21 +0900

    ✨ feat: genderIcon pathURL 상수 정의

---
+++ src/shared/const/UserprofileImgPath.ts
@@ -0,0 +1,5 @@
+export const DEFAULT_PROFILE_IMG_CAT = "/images/profile_cat.png";
+export const DEFAULT_PROFILE_IMG_DOG = "/images/profile_dog.png";
+
+export const FEMALE_ICON_IMG = "/icons/female.png";
+export const MALE_ICON_IMG = "/icons/male.png";
