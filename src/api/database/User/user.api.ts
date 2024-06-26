82fc303b12ffb632dc24b04d200df5996830dd73
Yujiii0 <yuji918@naver.com>
2024-06-26 13:39:42 +0900

    ✨ feat: userProfile API 구현

---
+++ src/api/database/User/user.api.ts
@@ -0,0 +1,28 @@
+import { UserProfile } from "firebase/auth";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { db } from "..";
+
+export const createUserProfile = async (
+  userData: UserProfile,
+  userId: string
+) => {
+  return await setDoc(doc(db, "users", userId), userData);
+};
+
+export const updateUserProfile = async () => {};
+
+export const getUserProfile = async (userId: string) => {
+  const docRef = doc(db, "users", userId);
+  const docSnap = await getDoc(docRef);
+  if (docSnap.exists()) {
+    const userProfileData = docSnap.data();
+    return userProfileData;
+  }
+};
+
+const userAPI = {
+  createUserProfile,
+  updateUserProfile,
+  getUserProfile,
+};
+export default userAPI;
