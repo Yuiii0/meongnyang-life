38d4876d2e16abee1491c9b05308305b280415d7
Yujiii0 <yuji918@naver.com>
2024-06-27 15:01:52 +0900

    🚚 rename: 팔로우, 언팔로우 컴포넌트명 변경

---
+++ src/components/pages/user/follow/FollowButton/FollowingButton.tsx
@@ -0,0 +1,16 @@
+interface FollowingButtonProps {
+  handleToggleFollowButton: () => void;
+}
+
+function FollowButton({ handleToggleFollowButton }: FollowingButtonProps) {
+  return (
+    <button
+      onClick={handleToggleFollowButton}
+      className="px-6 py-2 text-sm text-white border rounded-lg border-brand-100 active:border bg-brand-100 hover:border-brand-100 hover:bg-opacity-90 hover:transition-all active:border-brand-100 active:text-brand-100 active:bg-white "
+    >
+      팔로우
+    </button>
+  );
+}
+
+export default FollowButton;
--- src/components/pages/user/follow/FollowerButton.tsx
+++
@@ -1,9 +0,0 @@
-function FollowerButton() {
-  return (
-    <button className="py-2 text-sm transition-all border rounded-lg px-7 border-brand-100 text-brand-100 hover:bg-brand-100 hover:text-white active:bg-brand-100 active:bg-white">
-      팔로우
-    </button>
-  );
-}
-
-export default FollowerButton;
