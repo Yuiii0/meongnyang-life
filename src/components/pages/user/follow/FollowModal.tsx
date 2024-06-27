284e61c6a2cad3972d7796ad6e52d176bf58fdb9
Yujiii0 <yuji918@naver.com>
2024-06-27 23:26:04 +0900

    ✨ feat: FollowModal 팔로잉,팔로워 탭 기능 구현

--- src/components/pages/user/follow/FollowModal.tsx
+++ src/components/pages/user/follow/FollowModal.tsx
@@ -1,26 +1,48 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@radix-ui/react-tabs";
+import { useState } from "react";
+import UserCardsList from "../userList/UserCardsList";
 
-const count = 10;
-function FollowModal() {
-  // const [isFollowing, setIsFollowing] = useState(true);
+interface FollowModalProps {
+  followings: string[];
+  followers: string[];
+}
+function FollowModal({ followings, followers }: FollowModalProps) {
+  const [activeTab, setActiveTab] = useState("following");
+
+  const handleChangeTab = (value: string) => {
+    setActiveTab(value);
+  };
   return (
     <div className="w-screen ">
       <Tabs
         defaultValue="following"
         className="w-screen text-center text-gray-500"
+        onValueChange={handleChangeTab}
       >
         <TabsList className="flex justify-center ">
           <TabsTrigger
             value="following"
-            className="flex-1 py-3 border-b-2 border-brand-100 text-brand-100 opacity-95"
-          >{`팔로잉 ${count}`}</TabsTrigger>
+            className={`flex-1 py-3 border-b ${
+              activeTab === "following"
+                ? "border-b-2 border-brand-100 text-brand-100 opacity-95"
+                : ""
+            }`}
+          >{`팔로잉 ${followings.length}`}</TabsTrigger>
           <TabsTrigger
             value="follower"
-            className="flex-1 "
-          >{`팔로워 ${count}`}</TabsTrigger>
+            className={`flex-1  border-b ${
+              activeTab === "follower"
+                ? "border-b-2 border-brand-100 text-brand-100 opacity-95"
+                : ""
+            }`}
+          >{`팔로워 ${followers.length}`}</TabsTrigger>
         </TabsList>
-        <TabsContent value="following">팔로잉 리스트</TabsContent>
-        <TabsContent value="follower">팔로워 리스트</TabsContent>
+        <TabsContent value="following">
+          <UserCardsList userIdList={followings} />
+        </TabsContent>
+        <TabsContent value="follower">
+          <UserCardsList userIdList={followers} />
+        </TabsContent>
       </Tabs>
     </div>
   );
