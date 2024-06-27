68312f4577f1fee6b4683256b96416d1307ad0c2
Yujiii0 <yuji918@naver.com>
2024-06-27 23:27:11 +0900

    💄 design: 팔로우 버튼 레이아웃 수정

--- src/components/pages/user/profiles/UserProfileCard.tsx
+++ src/components/pages/user/profiles/UserProfileCard.tsx
@@ -12,6 +12,7 @@ import { useAuthStore } from "@/stores/auth/useAuthStore";
 import { UserRoundCog } from "lucide-react";
 import { Link } from "react-router-dom";
 import FollowToggleButton from "../follow/FollowButton/FollowToggleButton";
+import FollowModal from "../follow/FollowModal";
 
 interface UserProfileCardProps {
   userProfile: UserProfile;
@@ -40,24 +41,26 @@ function UserProfileCard({ userProfile }: UserProfileCardProps) {
             className="w-full h-full overflow-hidden"
           />
         </div>
-        <div className="flex-grow ml-4">
+        <div className="flex-grow ml-6">
           <div className="flex flex-col gap-y-1">
             <div className="flex justify-between">
-              <div className="text-lg font-semibold text-gray-900 ">
+              <div className="text-lg text-gray-900 ptfont-semibold ">
                 {nickName}
               </div>
-              {isMyProfile ? (
-                <div className="ml-auto">
-                  <Link
-                    to={`/profiles/update/${userId}`}
-                    className="text-brand-100"
-                  >
-                    <UserRoundCog size={20} />
-                  </Link>
-                </div>
-              ) : (
-                <FollowToggleButton userId={userId} />
-              )}
+              <div className="">
+                {isMyProfile ? (
+                  <div className="ml-auto">
+                    <Link
+                      to={`/profiles/update/${userId}`}
+                      className="text-brand-100"
+                    >
+                      <UserRoundCog size={20} />
+                    </Link>
+                  </div>
+                ) : (
+                  <FollowToggleButton userId={userId} />
+                )}
+              </div>
             </div>
             <div className="flex items-center gap-x-2">
               <span className="text-sm text-gray-500">{breed}</span>
@@ -77,6 +80,7 @@ function UserProfileCard({ userProfile }: UserProfileCardProps) {
       <div className="text-sm text-gray-600 whitespace-pre-wrap">
         {introduction}
       </div>
+      <FollowModal followers={followers || []} followings={followings || []} />
     </div>
   );
 }
