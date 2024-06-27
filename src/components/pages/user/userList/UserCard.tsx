3e167274f88b273987dba1509eb16fdbfcb06b45
Yujiii0 <yuji918@naver.com>
2024-06-27 23:24:46 +0900

    ✨ feat: userCardsList 구현

--- src/components/pages/user/userList/UserCard.tsx
+++ src/components/pages/user/userList/UserCard.tsx
@@ -1,16 +1,25 @@
+import { useGetUserProfile } from "@/hooks/User/useGetUserProfile";
 import { DEFAULT_PROFILE_IMG_CAT } from "@/shared/const/UserprofileImgPath";
-const nickname = "누리";
-const breed = "포메라니안";
+import FollowToggleButton from "../follow/FollowButton/FollowToggleButton";
 
-function UserCard() {
+interface UserCardProps {
+  userId: string;
+}
+
+function UserCard({ userId }: UserCardProps) {
+  const { data: userProfile } = useGetUserProfile(userId);
+  const nickName = userProfile?.nickName;
+  const breed = userProfile?.breed;
+
   return (
     <div className="flex items-center px-6 py-3 ">
       <div className="h-14 w-14">
         <img src={DEFAULT_PROFILE_IMG_CAT} />
       </div>
       <div className="pl-4">
-        <div className="font-semibold">{nickname}</div>
+        <div className="font-semibold">{nickName}</div>
         <div className="text-sm text-gray-500">{breed}</div>
+        <FollowToggleButton userId={userId} />
       </div>
       <div className="ml-auto"></div>
     </div>
