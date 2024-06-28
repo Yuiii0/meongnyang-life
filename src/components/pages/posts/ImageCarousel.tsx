ca7e45c3d713e01d8984170405364698cf7dfe66
Yujiii0 <yuji918@naver.com>
2024-06-28 20:12:32 +0900

    ✨ feat: 첨부된 이미지 파일 삭제 기능 추가

--- src/components/pages/posts/ImageCarousel.tsx
+++ src/components/pages/posts/ImageCarousel.tsx
@@ -3,12 +3,14 @@ import {
   CarouselContent,
   CarouselItem,
 } from "@/components/ui/carousel";
+import { X } from "lucide-react";
 
 interface ImageCarouselProps {
   images: string[];
+  onRemoveImage: (index: number) => void;
 }
 
-function ImageCarousel({ images }: ImageCarouselProps) {
+function ImageCarousel({ images, onRemoveImage }: ImageCarouselProps) {
   return (
     <Carousel
       opts={{
@@ -19,12 +21,18 @@ function ImageCarousel({ images }: ImageCarouselProps) {
       <CarouselContent>
         {images.map((image, index) => (
           <CarouselItem key={index} className="basis-1/3">
-            <div className="w-24 h-24 overflow-hidden max-h-28 max-w-28">
+            <div className="relative w-24 h-24 overflow-hidden max-h-28 max-w-28">
               <img
                 src={image}
                 alt={`image_${index + 1}`}
                 className="object-cover w-full h-full"
               />
+              <button
+                onClick={() => onRemoveImage(index)}
+                className="absolute top-0 right-0 text-gray-400 bg-gray-200 "
+              >
+                <X strokeWidth={1} />
+              </button>
             </div>
           </CarouselItem>
         ))}
