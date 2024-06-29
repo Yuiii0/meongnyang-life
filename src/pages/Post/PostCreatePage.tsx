f28f8520041885f268dcc49669cd74582d53ba8c
Yujiii0 <yuji918@naver.com>
2024-06-29 19:15:57 +0900

    ✨ feat: PostCreate 기능 추가

--- src/pages/Post/PostCreatePage.tsx
+++ src/pages/Post/PostCreatePage.tsx
@@ -1,10 +1,36 @@
 import PostForm from "@/components/pages/posts/PostForm";
 import Page from "@/components/ui/Page";
+import useCreatePost from "@/lib/post/hooks/useCreatePost";
+import { PostFormData } from "@/lib/post/type";
+import { useAuthStore } from "@/stores/auth/useAuthStore";
+import { Timestamp } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
 
 function PostCreatePage() {
+  const { user } = useAuthStore();
+
+  const { mutateAsync: create } = useCreatePost();
+  const navigate = useNavigate();
+  const handleCreatePost = async (data: PostFormData) => {
+    const postDto = {
+      userId: user?.uid || "",
+      title: data.title,
+      content: data.content,
+      images: data.images || null,
+      createdAt: Timestamp.now(),
+      likeCount: 0,
+      commentCount: 0,
+    };
+    try {
+      const postId = await create(postDto);
+      navigate(`/posts/${postId}`);
+    } catch (error) {
+      alert("포스트 작성해 실패하였습니다. 다시 시도해주세요");
+    }
+  };
   return (
     <Page>
-      <PostForm />
+      <PostForm onSubmit={handleCreatePost} />
     </Page>
   );
 }
