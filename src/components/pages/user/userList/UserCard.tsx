ef0b54ac552ae37323749a1fdd3f40ae579d0324
Yujiii0 <yuji918@naver.com>
2024-07-01 22:58:56 +0900

    🐛 fix: 시간 확장성 추가

--- src/components/pages/user/userList/UserCard.tsx
+++ src/components/pages/user/userList/UserCard.tsx
@@ -1,18 +1,27 @@
+import { useGetUserProfile } from "@/hooks/User/useGetUserProfile";
 import { DEFAULT_PROFILE_IMG_CAT } from "@/shared/const/UserprofileImgPath";
-const nickname = "누리";
-const breed = "포메라니안";
 
-function UserCard() {
+interface UserCardProps {
+  userId: string;
+  isDate?: string;
+}
+function UserCard({ userId, isDate }: UserCardProps) {
+  const { data: userProfile } = useGetUserProfile(userId || "");
+  console.log("Date", isDate);
   return (
-    <div className="flex items-center px-6 py-3 ">
-      <div className="h-14 w-14">
-        <img src={DEFAULT_PROFILE_IMG_CAT} />
+    <div>
+      <div className="flex items-center py-3">
+        <div className="h-14 w-14">
+          <img src={userProfile?.profileImg || DEFAULT_PROFILE_IMG_CAT} />
+        </div>
+        <div className="pl-4">
+          <div className="font-semibold">{userProfile?.nickName}</div>
+          <div className="text-sm text-gray-500">
+            {isDate ? isDate : userProfile?.breed}
+          </div>
+        </div>
+        <div className="ml-auto"></div>
       </div>
-      <div className="pl-4">
-        <div className="font-semibold">{nickname}</div>
-        <div className="text-sm text-gray-500">{breed}</div>
-      </div>
-      <div className="ml-auto"></div>
     </div>
   );
 }
