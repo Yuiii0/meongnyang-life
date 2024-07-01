dd92251d4bec01e0caef9e310becd7c14f6591a5
Yujiii0 <yuji918@naver.com>
2024-07-01 22:42:18 +0900

    ✨ feat: formatTimestamp util 함수 생성

---
+++ src/utils/formatTimestamp.ts
@@ -0,0 +1,24 @@
+import { Timestamp } from "firebase/firestore";
+export const formatTimestamp = (timestamp: Timestamp) => {
+  const date = new Date(
+    timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000
+  );
+  const now = new Date();
+  const diff = now.getTime() - date.getTime();
+
+  if (diff < 3600000) {
+    // 1시간 이내인 경우
+    const minutes = Math.floor(diff / 60000);
+    return `${minutes}분 전`;
+  } else if (diff < 86400000) {
+    // 24시간 이내인 경우
+    const hours = Math.floor(diff / 3600000);
+    return `${hours}시간 전`;
+  } else {
+    // 24시간 이후인 경우
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const year = String(date.getFullYear()).slice(2);
+    return `${day}.${month}.${year}`;
+  }
+};
