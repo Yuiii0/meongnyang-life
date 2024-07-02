472a9222192029bd2ee0b494cf0ee175cc0493bb
Yujiii0 <yuji918@naver.com>
2024-07-03 00:04:26 +0900

    ✨ ✨ feat: useDeletePostLikeReaction optimistic update 수정

--- src/lib/post/hooks/useDeletePostLikeReaction.tsx
+++ src/lib/post/hooks/useDeletePostLikeReaction.tsx
@@ -1,6 +1,6 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { getPostLikeCount, removePostLikeReaction } from "../api";
-import { POST, POST_LIKE_COUNT, POST_LIKE_STATUS } from "../key";
+import { removePostLikeReaction } from "../api";
+import { POST_LIKE_COUNT, POST_LIKE_STATUS } from "../key";
 
 export const useDeletePostLikeReaction = (postId: string, userId: string) => {
   const queryClient = useQueryClient();
@@ -8,15 +8,33 @@ export const useDeletePostLikeReaction = (postId: string, userId: string) => {
   return useMutation({
     mutationFn: async () => {
       await removePostLikeReaction(postId, userId);
-      return await getPostLikeCount(postId); // 최신 likeCount 반환
     },
-    onSuccess: (likeCount) => {
+    onMutate: async () => {
+      const previousLikeCount = queryClient.getQueryData([
+        POST_LIKE_COUNT,
+        postId,
+      ]);
+
+      queryClient.setQueryData<number | undefined>(
+        [POST_LIKE_COUNT, postId],
+        (old) => (old ? old - 1 : 0)
+      );
+
+      return { previousLikeCount };
+    },
+    onError: (_err, _variables, context) => {
+      if (context?.previousLikeCount) {
+        queryClient.setQueryData(
+          [POST_LIKE_COUNT, postId],
+          context.previousLikeCount
+        );
+      }
+    },
+    onSettled: () => {
       queryClient.invalidateQueries({
         queryKey: [POST_LIKE_STATUS, postId, userId],
       });
-      queryClient.invalidateQueries({ queryKey: [POST, postId] });
-      queryClient.setQueryData([POST_LIKE_COUNT, postId], likeCount); // 최신 likeCount 설정
+      queryClient.invalidateQueries({ queryKey: [POST_LIKE_COUNT, postId] });
     },
-    mutationKey: [POST_LIKE_STATUS, postId, userId], // mutationKey 설정
   });
 };
