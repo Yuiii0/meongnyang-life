63ba91ae9fdd4c9c48f68fb63e6c905915c2df3d
Yujiii0 <yuji918@naver.com>
2024-07-04 22:19:25 +0900

    ✨ feat: Comment QueryKey

--- src/lib/post/key.ts
+++ src/lib/post/key.ts
@@ -1,4 +1,7 @@
 export const POST = "POST";
+
 export const POST_LIKE_STATUS = "POST_LIKE_STATUS";
 export const POST_LIKE_COUNT = "POST_LIKE_COUNT";
 export const POST_LIKED_BY_USER = "POST_LIKED_BY_USER";
+
+export const COMMENT = "COMMENT";
