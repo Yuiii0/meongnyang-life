92184c0573c4f3572981d432ddb3281f7e78fa09
Yujiii0 <yuji918@naver.com>
2024-07-04 22:15:52 +0900

    ✨ feat: CommentItem 컴포넌트 구현

--- src/components/pages/posts/Comments/CommentItem.tsx
+++ src/components/pages/posts/Comments/CommentItem.tsx
@@ -1,19 +1,47 @@
+import { useGetUserProfile } from "@/hooks/User/useGetUserProfile";
+import useDeleteComment from "@/lib/post/hooks/comments/useDeleteComment";
+import { CommentDto } from "@/lib/post/type";
 import { DEFAULT_PROFILE_IMG_CAT } from "@/shared/const/UserprofileImgPath";
+import { useAuthStore } from "@/stores/auth/useAuthStore";
+import { formatTimestamp } from "@/utils/formatTimestamp";
+import { Timestamp } from "firebase/firestore";
 import { Heart } from "lucide-react";
+import React from "react";
 
-const nickName = "두부";
-const date = "23.05.29";
-const content =
-  "너무 귀엽다 ㅎㅎ 너무 귀엽다 ㅎㅎ너무 귀엽다 ㅎㅎ너무 귀엽다 너무 귀엽다 ㅎㅎ너무 귀엽다 너무 귀엽다 ㅎㅎ너무 귀엽다 ";
-const isMyComment = true;
+interface CommentItemProps {
+  comment: CommentDto;
+  isMyPost: boolean;
+}
 
-function CommentItem() {
+const CommentItem: React.FC<CommentItemProps> = ({ comment, isMyPost }) => {
+  console.log("commentITEM", comment);
+  const { user } = useAuthStore();
+  const { data: userInfo } = useGetUserProfile(comment.userId);
+  const isMyComment = comment.userId === user?.uid;
+
+  const { mutate: deleteComment } = useDeleteComment(
+    comment.postId,
+    comment.id || ""
+  );
+  const handleDeleteComment = () => {
+    try {
+      deleteComment();
+    } catch (error) {
+      alert("오류가 발생하였습니다. 다시 시도해주세요");
+    }
+  };
+
+  const timeStamp = new Timestamp(
+    comment.createdAt.seconds,
+    comment.createdAt.nanoseconds
+  );
+
   return (
     <div className="flex py-4">
       <div className="flex flex-1 gap-x-3">
         <div>
           <img
-            src={DEFAULT_PROFILE_IMG_CAT}
+            src={userInfo?.profileImg || DEFAULT_PROFILE_IMG_CAT}
             alt="profile-img"
             width={50}
             height={50}
@@ -21,17 +49,17 @@ function CommentItem() {
         </div>
         <div className="w-full">
           <div className="flex items-center gap-x-2">
-            <p className="font-semibold text-gray-900">{nickName}</p>
-            <p className="text-xs text-gray-400">{date}</p>
-            {isMyComment && (
-              <div className="flex items-start ml-auto mr-4 text-xs text-gray-500 gap-x-2">
-                <button>수정</button>
-                <button>삭제</button>
-              </div>
-            )}
+            <p className="font-semibold text-gray-900">{userInfo?.nickName}</p>
+            <p className="text-xs text-gray-400">
+              {formatTimestamp(timeStamp)}
+            </p>
+            <div className="flex items-start ml-auto mr-4 text-xs text-gray-500 gap-x-2">
+              {isMyComment && <button>수정</button>}
+              {isMyPost && <button onClick={handleDeleteComment}>삭제</button>}
+            </div>
           </div>
-          <p className="w-full pr-1 text-sm text-gray-600 whitespace-pre-wrap ">
-            {content}
+          <p className="w-full pr-1 text-sm text-gray-600 whitespace-pre-wrap">
+            {comment.content}
           </p>
         </div>
       </div>
@@ -40,6 +68,6 @@ function CommentItem() {
       </button>
     </div>
   );
-}
+};
 
 export default CommentItem;
