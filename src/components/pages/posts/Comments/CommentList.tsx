f65915deb80d252121656881bd26fb3f36fdacd7
Yujiii0 <yuji918@naver.com>
2024-07-05 20:51:45 +0900

    🐛 fix: React.memo 적용

--- src/components/pages/posts/Comments/CommentList.tsx
+++ src/components/pages/posts/Comments/CommentList.tsx
@@ -1,29 +1,33 @@
 import { CommentDto } from "@/lib/comment/type";
+import React from "react";
 import CommentItem from "./CommentItem";
+
 interface CommentListProps {
   comments: CommentDto[];
   isMyPost: boolean;
   onEditComment: (comment: CommentDto) => void;
 }
 
-function CommentList({ comments, isMyPost, onEditComment }: CommentListProps) {
-  return (
-    <ul>
-      {comments && comments.length > 0 ? (
-        comments.map((comment) => (
-          <li key={comment.id}>
-            <CommentItem
-              comment={comment}
-              isMyPost={isMyPost}
-              onEditComment={onEditComment}
-            />
-          </li>
-        ))
-      ) : (
-        <li>No comments available</li>
-      )}
-    </ul>
-  );
-}
+const CommentList: React.FC<CommentListProps> = React.memo(
+  ({ comments, isMyPost, onEditComment }) => {
+    return (
+      <ul>
+        {comments && comments.length > 0 ? (
+          comments.map((comment) => (
+            <li key={comment.id}>
+              <CommentItem
+                comment={comment}
+                isMyPost={isMyPost}
+                onEditComment={onEditComment}
+              />
+            </li>
+          ))
+        ) : (
+          <li>No comments available</li>
+        )}
+      </ul>
+    );
+  }
+);
 
 export default CommentList;
