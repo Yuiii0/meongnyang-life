0706845738a545df91a3967752279a6fe6adba80
Yujiii0 <yuji918@naver.com>
2024-07-05 16:10:46 +0900

    ✨ feat: useUpdateComment 훅 생성

---
+++ src/lib/comment/hooks/useUpdateComment.tsx
@@ -0,0 +1,42 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateComment } from "../api";
+import { COMMENT } from "../key";
+import { CommentDto } from "../type";
+
+export default function useUpdateComment(postId: string, commentId: string) {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (content: string) => updateComment(postId, commentId, content),
+    onMutate: async (content) => {
+      await queryClient.cancelQueries({ queryKey: [COMMENT, postId] });
+      const previousComments = queryClient.getQueryData([COMMENT, postId]);
+
+      queryClient.setQueryData(
+        [COMMENT, postId],
+        (old: { pages: CommentDto[][]; pageParams: any[] } | undefined) => {
+          if (!old) return { pages: [], pageParams: [] };
+          return {
+            ...old,
+            pages: old.pages.map((page) =>
+              page.map((comment) =>
+                comment.id === commentId
+                  ? { ...comment, content, updatedAt: new Date() }
+                  : comment
+              )
+            ),
+          };
+        }
+      );
+      return { previousComments };
+    },
+    onError: (_err, _variables, context) => {
+      if (context?.previousComments) {
+        queryClient.setQueryData([COMMENT, postId], context.previousComments);
+      }
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: [COMMENT, postId] });
+    },
+  });
+}
