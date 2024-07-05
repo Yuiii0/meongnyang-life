d5988f2c6f4563d75244d6c548a277c7c3d17a43
Yujiii0 <yuji918@naver.com>
2024-07-05 21:03:02 +0900

    ✏️ comment: 오타 수정

--- src/components/ui/LoadingSpinner.tsx
+++ src/components/ui/LoadingSpinner.tsx
@@ -5,7 +5,7 @@ interface LoadingSpinnerProps {
 function LoadingSpinner({ text }: LoadingSpinnerProps) {
   return (
     <div className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-white gap-y-2 bg-opacity-80">
-      <img src="images/loading.gif" alt="loading" width={180} height={48} />
+      <img src="/images/loading.gif" alt="loading" width={180} height={48} />
       {text && <p className="text-sm text-gray-500">{text}</p>}
     </div>
   );
