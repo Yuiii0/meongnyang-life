725041944a044374653655e151bf03ecb237e5fe
Yujiii0 <yuji918@naver.com>
2024-07-06 20:12:40 +0900

    🔧 chore: 목데이터 빌드 수정

--- src/components/pages/posts/Comments/LikeButton/CommentLikeToggleButton.tsx
+++ src/components/pages/posts/Comments/LikeButton/CommentLikeToggleButton.tsx
@@ -1,9 +1,8 @@
-import { useState } from "react";
 import CommentLikeButton from "./CommentLikeButton";
 import CommentUnLikeButton from "./CommentUnLikeButton";
 
 function CommentLikeToggleButton() {
-  const [isLike, setIsLike] = useState(false);
+  const isLike = false;
 
   return <div>{isLike ? <CommentUnLikeButton /> : <CommentLikeButton />}</div>;
 }
