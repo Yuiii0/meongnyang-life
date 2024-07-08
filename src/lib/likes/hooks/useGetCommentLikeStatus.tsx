3d27e46103feebdd048f301b6fb1f96192942ec2
Yujiii0 <yuji918@naver.com>
2024-07-08 14:14:59 +0900

    ✨ feat: useGetCommentLikeStatus 훅 생성

---
+++ src/lib/likes/hooks/useGetCommentLikeStatus.tsx
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+import { getCommentLikeStatus } from "../api";
+import { COMMENT_LIKE_STATUS } from "../key";
+
+export const useGetCommentLikeStatus = (
+  commentId: string,
+  userId: string,
+  type: "COMMENT" | "REPLY",
+  replyId?: string
+) => {
+  return useQuery({
+    queryKey: [COMMENT_LIKE_STATUS, commentId, replyId],
+    queryFn: () => getCommentLikeStatus(commentId, userId, type, replyId),
+    staleTime: 1000 * 60 * 1,
+  });
+};
