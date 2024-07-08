5a1dec334c0c4fa715bce555f451f5e682d75c56
Yujiii0 <yuji918@naver.com>
2024-07-08 13:37:23 +0900

    ✨ feat: CommentLike API 구현

--- src/lib/post/api.ts
+++ src/lib/post/api.ts
@@ -10,12 +10,10 @@ import {
   doc,
   getDoc,
   getDocs,
-  increment,
   limit,
   orderBy,
   query,
   serverTimestamp,
-  setDoc,
   startAfter,
   updateDoc,
   where,
@@ -137,69 +135,5 @@ export const getPostsByUserId = async (userId: string) => {
   querySnapshot.forEach((doc) => {
     posts.push(doc.data() as postDto);
   });
-  return posts;
-};
-
-// Like
-export const getPostLikeStatus = async (postId: string, userId: string) => {
-  const likePostDocRef = doc(db, `like_posts/${userId}/posts/${postId}`);
-  const docSnap = await getDoc(likePostDocRef);
-
-  return docSnap.exists();
-};
-
-export const createPostLikeReaction = async (
-  postId: string,
-  userId: string
-) => {
-  const likePostRef = doc(db, `like_posts/${userId}/posts/${postId}`);
-  await setDoc(likePostRef, {
-    userId,
-    postId,
-    createdAt: serverTimestamp(),
-  });
-  const postRef = doc(db, "posts", postId);
-  await updateDoc(postRef, {
-    likeCount: increment(1),
-  });
-};
-export const removePostLikeReaction = async (
-  postId: string,
-  userId: string
-) => {
-  const LikePostRef = doc(db, `like_posts/${userId}/posts/${postId}`);
-  await deleteDoc(LikePostRef);
-
-  const postRef = doc(db, "posts", postId);
-  await updateDoc(postRef, {
-    likeCount: increment(-1),
-  });
-};
-export const getPostLikeCount = async (postId: string) => {
-  const postRef = doc(db, "posts", postId);
-  const docSnap = await getDoc(postRef);
-  if (docSnap.exists()) {
-    const postData = docSnap.data() as postDto;
-    return postData.likeCount;
-  } else {
-    throw new Error("포스트를 찾을 수 없습니다.");
-  }
-};
-
-export const getLikedPostsByUserId = async (userId: string) => {
-  const likedPostsQuery = query(collection(db, `like_posts/${userId}/posts`));
-  const likedPostsSnapshot = await getDocs(likedPostsQuery);
-  const likedPostIds: string[] = [];
-
-  likedPostsSnapshot.forEach((doc) => {
-    likedPostIds.push(doc.id);
-  });
-
-  const posts: postDto[] = [];
-  for (const postId of likedPostIds) {
-    const post = await getPostByPostId(postId);
-    posts.push(post as postDto);
-  }
-
   return posts;
 };
