cb7a7813fa95d4d373d78607b8d862a31358e0b2
Yujiii0 <yuji918@naver.com>
2024-07-09 10:55:24 +0900

    💄 design: fullWidth padding 수정

--- src/components/ui/Page.tsx
+++ src/components/ui/Page.tsx
@@ -8,7 +8,7 @@ function Page({ children, fullWidth }: PropsWithChildren<PageProps>) {
   return (
     <main
       className={`flex flex-col items-stretch w-full ${
-        fullWidth ? "max-w-none px-0" : "max-w-screen-xl px-8"
+        fullWidth ? "max-w-none !px-0 !py-0" : "max-w-screen-xl px-8"
       } py-8 mx-auto grow`}
     >
       {children}
