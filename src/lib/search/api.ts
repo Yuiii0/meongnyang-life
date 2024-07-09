e28b31b59aa6a852ac38cdf1652a055451703856
Yujiii0 <yuji918@naver.com>
2024-07-09 16:15:03 +0900

    ✨ feat: searchPostsByTitle api 추가

--- src/lib/search/api.ts
+++ src/lib/search/api.ts
@@ -1,6 +1,8 @@
 import { db } from "@/api/database";
 import { cleaningText } from "@/utils/cleaningText";
+import { createKeyWords } from "@/utils/createKeywords";
 import { collection, getDocs, query, where } from "firebase/firestore";
+import { postDto } from "../post/type";
 import { UserProfile } from "../user/type";
 
 export const searchUsersByNickname = async (nickname: string) => {
@@ -26,6 +28,34 @@ export const searchUsersByNickname = async (nickname: string) => {
     return users;
   } catch (error) {
     console.warn("유저 검색에 실패하였습니다.");
+    throw error;
+  }
+};
+
+export const searchPostsByTitle = async (title: string) => {
+  try {
+    const cleanedTitle = cleaningText(title);
+    const keywords = createKeyWords([cleanedTitle]);
+    console.log("cleanedTitle", cleanedTitle);
+    console.log("keywords", keywords);
+
+    const postRef = collection(db, "posts");
+    const q = query(postRef, where("keywords", "array-contains-any", keywords));
+    const querySnapshot = await getDocs(q);
+
+    if (querySnapshot.empty) {
+      console.log("No posts found");
+      return null;
+    }
+
+    const posts: postDto[] = [];
+    querySnapshot.forEach((doc) => {
+      posts.push(doc.data() as postDto);
+    });
+
+    return posts;
+  } catch (error) {
+    console.warn(error);
     throw error;
   }
 };
