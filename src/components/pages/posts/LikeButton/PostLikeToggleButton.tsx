238c6610433fc2c11de91bdaf0b0f8b84d5cecbf
Yujiii0 <yuji918@naver.com>
2024-07-11 14:56:33 +0900

    🐛 fix: debounce import 수정

--- src/components/pages/posts/LikeButton/PostLikeToggleButton.tsx
+++ src/components/pages/posts/LikeButton/PostLikeToggleButton.tsx
@@ -1,13 +1,13 @@
 import { useAuthStore } from "@/stores/auth/useAuthStore";
 
+import { useCreatePostLikeReaction } from "@/lib/likes/hooks/useCreatePostLikeReaction";
+import { useDeletePostLikeReaction } from "@/lib/likes/hooks/useDeletePostLikeReaction";
+import { useGetPostLikeCount } from "@/lib/likes/hooks/useGetPostLikeCount";
+import { useGetPostLikeStatus } from "@/lib/likes/hooks/useGetPostLikeStatus";
 import { formatCount } from "@/utils/formatCount";
-import { debounce } from "lodash";
+import debounce from "lodash/debounce";
 import PostLikeButton from "./PostLikeButton";
 import PostUnLikeButton from "./PostUnLikeButton";
-import { useGetPostLikeStatus } from '@/lib/likes/hooks/useGetPostLikeStatus';
-import { useGetPostLikeCount } from '@/lib/likes/hooks/useGetPostLikeCount';
-import { useCreatePostLikeReaction } from '@/lib/likes/hooks/useCreatePostLikeReaction';
-import { useDeletePostLikeReaction } from '@/lib/likes/hooks/useDeletePostLikeReaction';
 
 interface LikeToggleButtonProps {
   postId: string;
