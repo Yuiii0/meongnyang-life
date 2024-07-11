812fe5090b5fc414bc8d704f6e150d06e2bf3639
Yujiii0 <yuji918@naver.com>
2024-07-11 13:46:11 +0900

    🔨 chore: manualChunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,49 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { visualizer } from "rollup-plugin-visualizer";
 import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      filename: "./dist/stats.html", 
+      gzipSize: true, 
+      brotliSize: true, 
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src/"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+          firebase: [
+            "firebase/firestore",
+            "firebase/storage",
+            "firebase/analytics",
+          ],
+          ui: [
+            "@radix-ui/react-alert-dialog",
+            "@radix-ui/react-dialog",
+            "@radix-ui/react-dropdown-menu",
+            "@radix-ui/react-label",
+            "@radix-ui/react-popover",
+            "@radix-ui/react-slot",
+            "@radix-ui/react-tabs",
+          ],
+          lodash: ["lodash", "lodash.debounce"],
+          emojiPicker: ["emoji-picker-react"],
+        },
+      },
     },
   },
 });
