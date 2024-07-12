0bec3794d43bf349cc69d2190a4a53adb5576b46
Yujiii0 <yuji918@naver.com>
2024-07-12 15:20:17 +0900

    ✨ feat: useResetPw 훅 생성

---
+++ src/lib/auth/hooks/useResetPW.tsx
@@ -0,0 +1,21 @@
+import { useMutation } from "@tanstack/react-query";
+import { FirebaseError } from "firebase/app";
+import { Auth, sendPasswordResetEmail } from "firebase/auth";
+import toast from "react-hot-toast";
+
+export default function useResetPw() {
+  return useMutation({
+    mutationFn: ({ auth, email }: { auth: Auth; email: string }) =>
+      sendPasswordResetEmail(auth, email),
+    onSuccess: () => {
+      toast("메일을 확인해주세요", { icon: "✉️" });
+    },
+    onError: (error) => {
+      if (error instanceof FirebaseError) {
+        toast.error("에러가 발생했습니다. 다시 시도해주세요.");
+      } else {
+        toast.error("에러가 발생했습니다. 다시 시도해주세요.");
+      }
+    },
+  });
+}
