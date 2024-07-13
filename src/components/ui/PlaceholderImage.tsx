2226d016819910f890dc72c0fea504babda58acb
Yujiii0 <yuji918@naver.com>
2024-07-13 21:22:02 +0900

    ♻️ refactor: viewObserver 추가

--- src/components/ui/PlaceholderImage.tsx
+++ src/components/ui/PlaceholderImage.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useInView } from "react-intersection-observer";
 
 interface PlaceholderImageProps {
   src: string;
@@ -12,35 +13,42 @@ interface PlaceholderImageProps {
 const PlaceholderImage: React.FC<PlaceholderImageProps> = ({
   src,
   alt,
-  loading = "lazy",
+  loading = "eager",
   className = "",
   srcSet,
   sizes,
 }) => {
   const [loaded, setLoaded] = useState(false);
 
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.5,
+  });
+
   const handleLoad = () => {
     setLoaded(true);
   };
 
   return (
-    <div className={`relative ${className} aspect-w-1 aspect-h-1`}>
+    <div ref={ref} className={`relative ${className} aspect-w-1 aspect-h-1`}>
       <div
         className={`absolute top-0 left-0 w-full h-full bg-gray-100 transition-opacity duration-300 rounded-md ${
           loaded ? "opacity-0" : "opacity-100"
         }`}
       ></div>
-      <img
-        src={src}
-        srcSet={srcSet}
-        sizes={sizes}
-        alt={alt}
-        onLoad={handleLoad}
-        className={`absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-300 rounded-md ${
-          loaded ? "opacity-100" : "opacity-0"
-        }`}
-        loading={loading}
-      />
+      {inView && (
+        <img
+          src={src}
+          srcSet={srcSet}
+          sizes={sizes}
+          alt={alt}
+          onLoad={handleLoad}
+          className={`absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-300 rounded-md ${
+            loaded ? "opacity-100" : "opacity-0"
+          }`}
+          loading={loading}
+        />
+      )}
     </div>
   );
 };
