cc9f1b76220f36700a2b4b030f993d6db46d64a3
Yujiii0 <yuji918@naver.com>
2024-07-15 18:35:26 +0900

    💄 design: profile form layout

--- src/components/pages/user/profiles/OptionalProfileForm.tsx
+++ src/components/pages/user/profiles/OptionalProfileForm.tsx
@@ -19,80 +19,87 @@ function OptionalProfileForm() {
   }, [isNoPet, setValue]);
 
   return (
-    <div className="flex flex-col mb-2 gap-y-6">
-      <h4 className="px-1 py-2 text-sm font-semibold">반려동물 종류</h4>
-      <div className="flex flex-col items-start gap-y-4">
-        <div className="flex justify-center w-full gap-x-6">
-          <button
-            type="button"
-            className={`p-4 rounded-lg border-2 w-28 h-28 min-w-28 min-h-28 flex justify-center items-center flex-col ${
-              petType === "dog"
-                ? "border-orange-200 bg-orange-200 "
-                : "border-orange-200"
-            } transition-all duration-200`}
-            onClick={() => {
-              setValue("petType", "dog");
-              setValue("isNoPet", false);
-            }}
-          >
-            <img
-              src="/images/dog_smiling.webp"
-              alt="강아지"
-              className="object-contain mx-auto"
-              width={76}
-              height={56}
-            />
-            <p className="pt-1 text-xs font-semibold text-gray-700">강아지</p>
-          </button>
-          <button
-            type="button"
-            className={`p-4 rounded-lg border-2 w-28 h-28 min-w-28 min-h-28 flex justify-center items-center flex-col ${
-              petType === "cat"
-                ? "border-orange-200 bg-orange-200 "
-                : "border-orange-200"
-            } transition-all duration-200`}
-            onClick={() => {
-              setValue("petType", "cat");
-              setValue("isNoPet", false);
-            }}
-          >
-            <img
-              src="/images/cat_smiling.webp"
-              alt="고양이"
-              className="object-cover h-18 w-18"
-              width={76}
-              height={56}
-            />
-            <p className="pt-1 text-xs font-semibold text-gray-700">고양이</p>
-          </button>
+    <section>
+      <h4 className="px-1 py-2 pt-4 mb-2 text-sm font-semibold">
+        반려동물 종류
+      </h4>
+      <div className="flex flex-col mb-2 gap-y-10">
+        <div className="flex flex-col items-start gap-y-4">
+          <div className="flex justify-center w-full gap-x-6">
+            <button
+              type="button"
+              className={`p-4 rounded-lg border-2 w-28 h-28 min-w-28 min-h-28 flex justify-center items-center flex-col ${
+                petType === "dog"
+                  ? "border-orange-200 bg-orange-200 "
+                  : "border-orange-200"
+              } transition-all duration-200`}
+              onClick={() => {
+                setValue("petType", "dog");
+                setValue("isNoPet", false);
+              }}
+            >
+              <img
+                src="/images/dog_smiling.webp"
+                alt="강아지"
+                className="object-contain mx-auto"
+                width={76}
+                height={56}
+              />
+              <p className="pt-1 text-xs font-semibold text-gray-700">강아지</p>
+            </button>
+            <button
+              type="button"
+              className={`p-4 rounded-lg border-2 w-28 h-28 min-w-28 min-h-28 flex justify-center items-center flex-col ${
+                petType === "cat"
+                  ? "border-orange-200 bg-orange-200 "
+                  : "border-orange-200"
+              } transition-all duration-200`}
+              onClick={() => {
+                setValue("petType", "cat");
+                setValue("isNoPet", false);
+              }}
+            >
+              <img
+                src="/images/cat_smiling.webp"
+                alt="고양이"
+                className="object-cover h-18 w-18"
+                width={76}
+                height={56}
+              />
+              <p className="pt-1 text-xs font-semibold text-gray-700">고양이</p>
+            </button>
+          </div>
+          <div className="ml-8 ">
+            <label
+              className="inline-flex items-center"
+              htmlFor="yellowCheckBox"
+            >
+              <input
+                id="yellowCheckBox"
+                type="checkbox"
+                className="w-4 h-4 accent-orange-400"
+                {...register("isNoPet")}
+              />
+              <span className="pl-2 text-sm text-gray-400">
+                반려동물이 없어요
+              </span>
+            </label>
+          </div>
         </div>
-        <div className="mt-4 ml-8">
-          <label className="inline-flex items-center" htmlFor="yellowCheckBox">
-            <input
-              id="yellowCheckBox"
-              type="checkbox"
-              className="w-4 h-4 accent-orange-400"
-              {...register("isNoPet")}
-            />
-            <span className="pl-2 text-sm text-gray-400">
-              반려동물이 없어요
-            </span>
-          </label>
-        </div>
-      </div>
 
-      <SelectedBreed
-        petType={petType}
-        breed={breed}
-        handleChangeBreed={(value) => setValue("breed", value)}
-        disabled={isNoPet}
-      />
-      <SelectedGender
-        gender={gender}
-        handleChangeGender={(value) => setValue("gender", value)}
-        disabled={isNoPet}
-      />
-    </div>
+        <SelectedBreed
+          petType={petType}
+          breed={breed}
+          handleChangeBreed={(value) => setValue("breed", value)}
+          disabled={isNoPet}
+        />
+        <SelectedGender
+          gender={gender}
+          handleChangeGender={(value) => setValue("gender", value)}
+          disabled={isNoPet}
+        />
+      </div>
+    </section>
   );
 }
 export default OptionalProfileForm;
