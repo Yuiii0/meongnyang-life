083e19985b0fbe417944cbc20d87287d84f9449f
Yujiii0 <yuji918@naver.com>
2024-07-15 18:33:57 +0900

    ♻️ refactor: center position 추가

--- src/components/ui/Page.tsx
+++ src/components/ui/Page.tsx
@@ -2,14 +2,17 @@ import { PropsWithChildren } from "react";
 
 interface PageProps {
   fullWidth?: boolean;
+  isCenter?: boolean;
 }
 
-function Page({ children, fullWidth }: PropsWithChildren<PageProps>) {
+function Page({ children, fullWidth, isCenter }: PropsWithChildren<PageProps>) {
   return (
     <main
       className={`flex flex-col pt-24 items-stretch w-full ${
         fullWidth ? "max-w-none !px-0 !py-0 !pt-24" : "max-w-screen-xl px-8"
-      } py-8 mx-auto grow`}
+      } py-8 mx-auto grow ${
+        isCenter ? "items-center justify-center min-h-screen" : ""
+      }`}
     >
       {children}
     </main>
