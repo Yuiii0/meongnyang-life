100705469da1683e3490e9098f62b9659e2f8555
Yujiii0 <yuji918@naver.com>
2024-07-15 21:55:10 +0900

    💄 design: simpleCard styling

--- src/components/pages/posts/SimplePostCard.tsx
+++ src/components/pages/posts/SimplePostCard.tsx
@@ -19,35 +19,35 @@ function SimplePostCard({ post }: SimplePostCardProps) {
   return (
     <Link
       to={`/posts/${post.id}`}
-      className="flex items-center justify-between h-40 gap-x-4 "
+      className="flex items-center justify-between h-40 p-4 border-b gap-x-4"
     >
       <div className="flex flex-col flex-grow gap-y-2">
-        <h2 className="pb-1.pt-5 font-semibold text-left text-gray-700">
+        <h2 className="pb-1.pt-5 font-semibold text-left text-gray-700 text-[15px]">
           {post.title.slice(0, 16)}
         </h2>
-        <div className="text-sm text-left text-gray-600 whitespace-pre-wrap min-h-2">
-          {truncateString(post.content, 90)}
+        <div className="h-16 overflow-hidden text-sm text-left text-gray-600 whitespace-pre-wrap ">
+          {truncateString(post.content, 40)}
         </div>
-        <div className="flex justify-between">
+        <div className="flex items-start justify-between">
           <p className="text-sm font-semibold text-gray-500">{post.nickname}</p>
-          <div className="flex items-center gap-x-4">
+          <div className="flex items-center gap-x-4 ">
             <div onClick={handleButtonClick}>
-              <PostLikeToggleButton postId={post.id || ""} />
+              <PostLikeToggleButton postId={post.id || ""} size="small" />
             </div>
             <div className="flex items-center text-gray-600 gap-x-2">
-              <MessageSquare strokeWidth={1.5} />
+              <MessageSquare strokeWidth={1.5} size={18} />
               <span>{formatCount(post.commentCount || 0)}</span>
             </div>
           </div>
         </div>
       </div>
       {post.images && post.images.length > 0 && (
-        <div className="flex-shrink-0 ml-2 overflow-hidden rounded-md h-28 w-28">
+        <div className="flex-shrink-0 w-[120px] h-[120px] ml-2 overflow-hidden rounded-md">
           <PlaceholderImage
             src={post.images[0].small}
             srcSet={`${post.images[0].small} 400w, ${post.images[0].large} 1080w`}
             sizes="(max-width: 600px) 480px, 1080px"
-            className="object-cover w-full h-full"
+            className="object-cover w-full h-full "
             alt={`${post.title} img`}
             loading="lazy"
           />
