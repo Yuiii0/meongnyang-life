2e77a1de81ae854ede31baa148f3a366973049af
Yujiii0 <yuji918@naver.com>
2024-07-15 16:31:10 +0900

    🐛 fix: React.memo 수정

--- src/components/pages/posts/Comments/CommentList.tsx
+++ src/components/pages/posts/Comments/CommentList.tsx
@@ -1,7 +1,7 @@
 import LoadingSpinner from "@/components/ui/LoadingSpinner";
 import { useGetCommentsByPostId } from "@/lib/comment/hooks/useGetCommentsByPostId";
 import { CommentDto, ReplyDto } from "@/lib/comment/type";
-import React, { useEffect, useMemo } from "react";
+import { useEffect, useMemo } from "react";
 import { useInView } from "react-intersection-observer";
 import CommentItem from "./CommentItem";
 
@@ -13,46 +13,50 @@ interface CommentListProps {
   onSubmitReply: (commentId: string) => void;
 }
 
-const CommentList: React.FC<CommentListProps> = React.memo(
-  ({ postId, isMyPost, onEditComment, onSubmitReply, onEditReply }) => {
-    const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
-      useGetCommentsByPostId(postId || "");
-    const comments = useMemo(
-      () => data?.pages.flatMap((page) => page) || [],
-      [data]
-    );
+function CommentList({
+  postId,
+  isMyPost,
+  onEditComment,
+  onSubmitReply,
+  onEditReply,
+}: CommentListProps) {
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useGetCommentsByPostId(postId || "");
+  const comments = useMemo(
+    () => data?.pages.flatMap((page) => page) || [],
+    [data]
+  );
 
-    const { ref, inView } = useInView({
-      threshold: 0.8,
-    });
+  const { ref, inView } = useInView({
+    threshold: 0.8,
+  });
 
-    useEffect(() => {
-      if (inView && hasNextPage) {
-        fetchNextPage();
-      }
-    }, [inView, hasNextPage, fetchNextPage]);
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, fetchNextPage]);
 
-    return (
-      <ul>
-        {comments.map((comment) => (
-          <li key={comment.id}>
-            <CommentItem
-              comment={comment as CommentDto}
-              isMyPost={isMyPost}
-              onEditComment={onEditComment}
-              onEditReply={onEditReply}
-              onSubmitReply={onSubmitReply}
-            />
-            <div ref={ref}>
-              {isFetchingNextPage && (
-                <LoadingSpinner text="댓글을 가져오는 중입니다 🐾" />
-              )}
-            </div>
-          </li>
-        ))}
-      </ul>
-    );
-  }
-);
+  return (
+    <ul>
+      {comments.map((comment) => (
+        <li key={comment.id}>
+          <CommentItem
+            comment={comment as CommentDto}
+            isMyPost={isMyPost}
+            onEditComment={onEditComment}
+            onEditReply={onEditReply}
+            onSubmitReply={onSubmitReply}
+          />
+          <div ref={ref}>
+            {isFetchingNextPage && (
+              <LoadingSpinner text="댓글을 가져오는 중입니다 🐾" />
+            )}
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
 
 export default CommentList;
