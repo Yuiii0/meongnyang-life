8ce7f44937e97c8b9046883b5c7b02acbf7b29f8
Yujiii0 <yuji918@naver.com>
2024-07-15 16:31:34 +0900

    ♻️ refactor: 컴포넌트 분리

--- src/components/pages/search/SearchResultTab.tsx
+++ src/components/pages/search/SearchResultTab.tsx
@@ -1,9 +1,8 @@
 import { PostDto } from "@/lib/post/type";
 import { formatCount } from "@/utils/formatCount";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@radix-ui/react-tabs";
-import SimplePostCardsList from "../posts/SimplePostCardsList";
-import UserCard from "../user/userList/UserCard";
-import NoResults from "./NoResults";
+import PostResults from "./PostResults";
+import UserResults from "./UserResults";
 
 interface SearchResultTabProps {
   initialTab: string;
@@ -47,35 +46,10 @@ function SearchResultTab({
         >{`게시글 ${formatCount(postData.length || 0)}`}</TabsTrigger>
       </TabsList>
       <TabsContent value="users" className="h-full overflow-auto">
-        {userIds && userIds.length > 0 ? (
-          <ul className="px-8 py-6">
-            {userIds.map((userId) => (
-              <li
-                key={userId}
-                className="flex items-center justify-between pb-1"
-              >
-                <UserCard userId={userId} />
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <NoResults
-            title="검색 결과가 없습니다."
-            description="다른 키워드로 검색해보세요"
-          />
-        )}
+        <UserResults userIds={userIds} />
       </TabsContent>
       <TabsContent value="posts" className="h-full overflow-auto">
-        <div className="px-8 py-6">
-          {postData && postData.length > 0 ? (
-            <SimplePostCardsList posts={postData} />
-          ) : (
-            <NoResults
-              title="검색 결과가 없습니다."
-              description="다른 키워드로 검색해보세요"
-            />
-          )}
-        </div>
+        <PostResults postData={postData} />
       </TabsContent>
     </Tabs>
   );
