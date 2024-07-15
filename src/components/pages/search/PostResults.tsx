c7d8b99d6443aaad9e8a1aecf6308b06e9ea4108
Yujiii0 <yuji918@naver.com>
2024-07-15 15:53:32 +0900

    ♻️ refactor: 컴포넌트 분리

---
+++ src/components/pages/search/PostResults.tsx
@@ -0,0 +1,22 @@
+import { PostDto } from "@/lib/post/type";
+import SimplePostCardsList from "../posts/SimplePostCardsList";
+import NoResults from "./NoResults";
+
+interface PostResultsProps {
+  postData: PostDto[];
+}
+
+function PostResults({ postData }: PostResultsProps) {
+  return postData && postData.length > 0 ? (
+    <div className="px-8 py-6">
+      <SimplePostCardsList posts={postData} />
+    </div>
+  ) : (
+    <NoResults
+      title="검색 결과가 없습니다."
+      description="다른 키워드로 검색해보세요"
+    />
+  );
+}
+
+export default PostResults;
---
+++ src/components/pages/search/UserResults.tsx
@@ -0,0 +1,25 @@
+import UserCard from "../user/userList/UserCard";
+import NoResults from "./NoResults";
+
+interface UserResultsProps {
+  userIds: string[];
+}
+
+function UserResults({ userIds }: UserResultsProps) {
+  return userIds && userIds.length > 0 ? (
+    <ul className="px-8 py-6">
+      {userIds.map((userId) => (
+        <li key={userId} className="flex items-center justify-between pb-1">
+          <UserCard userId={userId} />
+        </li>
+      ))}
+    </ul>
+  ) : (
+    <NoResults
+      title="검색 결과가 없습니다."
+      description="다른 키워드로 검색해보세요"
+    />
+  );
+}
+
+export default UserResults;
