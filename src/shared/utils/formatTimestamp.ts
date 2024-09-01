c33822b1c18ec27b3423f9247e66616639c8ebd2
Yujiii0 <yuji918@naver.com>
2024-09-01 19:26:50 +0900

    🐛 fix: formatTimeStamp 수정

--- src/shared/utils/formatTimestamp.ts
+++ src/shared/utils/formatTimestamp.ts
@@ -7,22 +7,25 @@ export const formatTimestamp = (timestamp: Timestamp) => {
   const now = new Date();
   const diff = now.getTime() - date.getTime();
 
-  if (diff < 60000) {
-    // 1분 이내인 경우
-    return "방금 전";
-  } else if (diff < 3600000) {
-    // 1시간 이내인 경우
-    const minutes = Math.floor(diff / 60000);
-    return `${minutes}분 전`;
-  } else if (diff < 86400000) {
-    // 24시간 이내인 경우
-    const hours = Math.floor(diff / 3600000);
-    return `${hours}시간 전`;
-  } else {
-    // 24시간 이후인 경우
-    const day = String(date.getDate()).padStart(2, "0");
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const year = String(date.getFullYear()).slice(2);
-    return `${year}.${month}.${day}`;
+  switch (true) {
+    case diff < 60000: // 1분 이내
+      return "방금 전";
+    case diff < 3600000: {
+      // 1시간 이내
+      const minutes = Math.floor(diff / 60000);
+      return `${minutes}분 전`;
+    }
+    case diff < 86400000: {
+      // 24시간 이내
+      const hours = Math.floor(diff / 3600000);
+      return `${hours}시간 전`;
+    }
+    default: {
+      // 24시간 이후
+      const day = String(date.getDate()).padStart(2, "0");
+      const month = String(date.getMonth() + 1).padStart(2, "0");
+      const year = String(date.getFullYear()).slice(2);
+      return `${year}.${month}.${day}`;
+    }
   }
 };
