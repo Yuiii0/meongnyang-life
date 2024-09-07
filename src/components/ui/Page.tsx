761035ccf580cbc91504e7e50d0477b975d80de7
Yujiii0 <yuji918@naver.com>
2024-09-07 14:25:50 +0900

    💄 design: 데스크탑 padding

--- src/components/ui/Page.tsx
+++ src/components/ui/Page.tsx
@@ -9,7 +9,9 @@ function Page({ children, fullWidth, isCenter }: PropsWithChildren<PageProps>) {
   return (
     <main
       className={`flex flex-col pt-24 items-center w-full mx-auto ${
-        fullWidth ? "max-w-none !px-0 !py-0 !pt-24" : "max-w-screen-xl px-6"
+        fullWidth
+          ? "max-w-none !px-0 !py-0 !pt-24"
+          : "max-w-screen-xl px-6 md:px-8"
       } py-8 grow ${
         isCenter ? "justify-center min-h-screen" : "items-stretch"
       }`}
